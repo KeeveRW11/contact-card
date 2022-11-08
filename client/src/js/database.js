c54782ce3f4c3f441ce8809826064d7be1f32bdf
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-08 01:58:19 -0500

    forms populate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,8 +1,8 @@
 import { openDB } from 'idb';
 import 'regenerator-runtime/runtime';
 
-
-export const initdb = async () =>
+//init database
+export const initDb = async () =>
     openDB('contact_db', 1, {
         upgrade(db) {
             if (db.objectStoreNames.contains('contacts')) {
@@ -14,3 +14,43 @@ export const initdb = async () =>
         }
     }
 );
+//get database
+export const getDb = async () => {
+  console.log('GET from the database');
+      // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('contact_db', 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction('contacts', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('contacts');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+// Export a function we will use to POST to the database.
+export const postDb = async (name, email, phone, profile)  => {
+  console.log('Post to the database');
+  
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('contact_db', 1);
+  
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction('contacts', 'readwrite');
+  
+  // Open up the desired object store.
+  const store = tx.objectStore('contacts');
+  
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ name: name, email: email, phone: phone, profile: profile});
+ 
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
