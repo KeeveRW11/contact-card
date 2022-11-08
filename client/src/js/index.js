db8840c0c1191b9ef35287dfd2fa93f7f1e59aab
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-08 01:12:24 -0500

    set index db

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,3 +1,6 @@
+//Import functions
+import { initdb } from "./database";
+
 //This imports the javascript modules
 import "./form";
 import "./submit";
@@ -14,7 +17,9 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
